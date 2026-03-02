041a57f2b799fed3fae61f7f3c73a36bc40db523
mavor <ma_voroshilov@mail.ru>
2026-03-02 21:55:48 +0300

    Fix issues

--- task2/nulldump.c
+++ task2/nulldump.c
@@ -1,29 +1,29 @@
-#include <linux/module.h>	/* Для всех модулей	*/
-#include <linux/kernel.h>	/* KERN_INFO		*/
-#include <linux/init.h>		/* Макросы		*/
-#include <linux/fs.h>		/* Макросы для устройств	*/
-#include <linux/cdev.h>		/* Функции регистрации символьных устройств	*/
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("MaxVorosh");
 MODULE_DESCRIPTION("/dev/null with dumps");
 MODULE_VERSION("0.1");
 
-#define DEVICE_NAME  "nulldump"
+#define DEVICE_NAME "nulldump"
 #define CLASS_NAME  "nulldump_class"
+#define HEXDUMP_LEN 32
 
 static int major;
 static dev_t dev;
 static struct class *chrdev_class;
 static struct device *sdev;
 
-void hexdump(const char *prefix, const void *buf, size_t len)
+void hexdump(const void *buf)
 {
-	for (int i = 0; i < len; i++) {
+	for (int i = 0; i < HEXDUMP_LEN; i++) {
 		if (!(i % 16)) {
 			if (i)
 				pr_info("\n");
-			pr_info("%s", prefix);
 		}
 		if (i && !(i % 8) && (i % 16))
 			pr_info("\t");
@@ -39,12 +39,23 @@ static ssize_t nulldump_read(struct file *file, char __user *buf, size_t len, lo
 
 static ssize_t nulldump_write(struct file *file, const char __user *buf, size_t len, loff_t *off)
 {
-	pr_info("chrdev: write with size=%d by pid=%d (%s)\n", size, current->pid, current->comm);
-    hexdump();
+	pr_info("chrdev: write with size=%d by pid=%d (%s)\n", len, current->pid, current->comm);
+	const char* data[HEXDUMP_LEN];
+	int bytes_to_copy = HEXDUMP_LEN;
+	for (int i = 0; i < len; i+=HEXDUMP_LEN) {
+		if (i + HEXDUMP_LEN >= len) {
+			bytes_to_copy = len - i;
+		}
+		int result = copy_from_user(data, buf + i, len);
+		if (result != 0) {
+			return -EFAULT;
+		}
+		hexdump(data);
+	}
 	return len;
 }
 
-static const struct file_operations chrdev_fops = {
+static const struct file_operations nulldump_fops = {
 	.owner  = THIS_MODULE,
 	.read   = nulldump_read,
 	.write  = nulldump_write,
@@ -53,27 +64,28 @@ static const struct file_operations chrdev_fops = {
 static int __init nulldump_init(void)
 {
 	// "If major == 0 this functions will dynamically allocate a major and return its number."
-    major = register_chrdev(0, DEVICE_NAME);
+    major = register_chrdev(0, DEVICE_NAME, &nulldump_fops);
     if (major < 0) {
-        pr_error("Cannor register chrdev nulldump");
+        pr_err("Cannor register chrdev nulldump");
         return major;
     }
 
+	int ret;
     if (IS_ERR(chrdev_class = class_create(CLASS_NAME)))
 	{
-		pr_err("chrdev: class_create failed\n");
+		pr_err("nulldump: class_create failed\n");
 		ret = PTR_ERR(chrdev_class);
 		goto err_unregister_chrdev;
 	}
 
 	if (IS_ERR(sdev = device_create(chrdev_class, NULL, dev, NULL, DEVICE_NAME))) 
 	{
-		pr_err("chrdev: device_create failed\n");
+		pr_err("nulldump: device_create failed\n");
 		ret = PTR_ERR(sdev);
 		goto err_class_destroy;
 	}
 
-	pr_info("chrdev: module loaded\n");
+	pr_info("nulldump: module loaded\n");
 	return 0;
 
 err_class_destroy:
