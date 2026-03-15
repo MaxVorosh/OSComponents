ef8625ead5ee4d79c6bb65658eb48ab68a9d0c63
mavor <ma_voroshilov@mail.ru>
2026-03-15 21:26:10 +0300

    Add device creation

--- task3/membuf.c
+++ task3/membuf.c
@@ -12,34 +12,51 @@ MODULE_VERSION("0.1");
 #define DEVICE_NAME "membuf"
 #define CLASS_NAME  "membuf_class"
 
-static dev_t dev;
-static struct cdev chrdev_cdev;
-static struct class *chrdev_class;
-static struct device *sdev;
+static struct class *membuf_class;
+static dev_t module_dev;
 
-static size_t base_device_size;
-static size_t devices_number;
+static size_t base_device_size = 4096;
+static size_t devices_number = 1;
+static const size_t MAX_DEVICES = 16;
 
-static ssize_t base_device_size_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf) {
-    return sprintf(buf, "%d\n", base_device_size);
-}
+static struct device_info {
+	static struct cdev cdev;
+	static struct device *sdev;
+	static dev_t dev;
 
-static ssize_t base_device_size_store(struct kobject *kobj, struct kobj_attribute *attr, char *buf, size_t count) {
-    sscanf(buf, "%du", &base_device_size);
-    return count;
-}
+	char* data;
+	size_t size;
 
-static ssize_t devices_number_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf) {
+	struct rw_semaphore lock;
+	atomic_t opened_by;
+};
+
+static device_info devices[MAX_DEVICES];
+static DEFINE_MUTEX(global_lock);
+
+static int create_membuf_device(int minor);
+static void destroy_membuf_device(int minor);
+
+static ssize_t devices_number_show(char *buffer, struct kernel_param *kp) {
     return sprintf(buf, "%d\n", devices_number);
 }
 
-static ssize_t devices_number_store(struct kobject *kobj, struct kobj_attribute *attr, char *buf, size_t count) {
+static ssize_t devices_number_store(const char *val, struct kernel_param *kp) {
     sscanf(buf, "%du", &devices_number);
     return count;
 }
 
-DEVICE_ATTR(base_device_size, 0660, base_device_size_show, (void *)base_device_size_store); 
-DEVICE_ATTR(devices_number, 0660, devices_number_show, (void *)devices_number_store); 
+static struct kernel_param_ops devices_number_ops = {
+	.get = devices_number_show,
+	.set = devices_number_store
+};
+
+static ssize_t size_show(struct device *dev, struct device_attribute *attr, const char *buf) {}
+static ssize_t size_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {}
+
+module_param(base_device_size, int, 0644);
+module_param_cb(devices_number, &devices_number_ops, &devices_number, 0644);
+DEVICE_ATTR(size, 0644, size_show, size_store);
 
 static ssize_t membuf_read(struct file *file, char __user *buf, size_t len, loff_t *off)
 {
@@ -59,58 +76,105 @@ static const struct file_operations membuf_fops = {
 	.write  = membuf_write,
 };
 
-static int __init membuf_init(void)
-{
-	int ret;
-    if ((ret = alloc_chrdev_region(&dev, 0, 1, DEVICE_NAME)))
-	{
-		pr_err("membuf: failed to allocate device number\n");
-		return ret;
+static int create_membuf_device(int minor) {
+	dev_t dev = MKDEV(MAJOR(module_dev), minor);
+	devices[minor].dev = dev;
+	devices[minor].data = kzalloc(base_device_size, GPF_KERNEL);
+	if (!devices[minor].data) {
+		return -ENOMEM;
 	}
-	pr_info("membuf: registered (major=%d, minor=%d)\n", MAJOR(dev), MINOR(dev));
+	devices[minor].size = base_device_size;
+	init_rwsem(&devices[minor].lock);
+	atomic_set(&devices[minor].opened_by, 0);
 
-	cdev_init(&chrdev_cdev, &membuf_fops);
-	chrdev_cdev.owner = THIS_MODULE;
+	cdev_init(&devices[minor].cdev, &membuf_fops);
+	devices[minor].cdev.owner = THIS_MODULE;
 
-	if ((ret = cdev_add(&chrdev_cdev, dev, 1)))
-	{
-		pr_err("membuf: cdev_add failed\n");
-		goto err_unregister;
+	int ret = cdev_add(&devices[minor].cdev, dev, 1);
+	if (ret < 0) {
+		goto free_buffer;
 	}
-
-    if (IS_ERR(chrdev_class = class_create(CLASS_NAME)))
-	{
-		pr_err("membuf: class_create failed\n");
-		ret = PTR_ERR(chrdev_class);
-		goto err_cdev_del;
+	devices[minor].sdev = device_create(membuf_class, NULL, dev, &devices[minor], "membuf_dev%d", minor);
+	if (IS_ERR(devices[minor].sdev)) {
+		ret = PTR_ERR(devices[minor].sdev);
+		goto delete_cdev;
 	}
-
-	if (IS_ERR(sdev = device_create(chrdev_class, NULL, dev, NULL, DEVICE_NAME))) 
-	{
-		pr_err("membuf: device_create failed\n");
-		ret = PTR_ERR(sdev);
-		goto err_class_destroy;
+	ret = device_create_file(devices[minor].sdev, &dev_attr_size);
+	if (ret < 0) {
+		device_destroy(membuf_class, devices[minor].dev);
+		goto delete_cdev;
 	}
-
-	pr_info("membuf: module loaded\n");
 	return 0;
 
-err_class_destroy:
-	class_destroy(chrdev_class);
-err_cdev_del:
-	cdev_del(&chrdev_cdev);
-err_unregister:
-	unregister_chrdev_region(dev, 1);
+delete_cdev:
+	cdev_del(devices[minor].cdev);
+free_buffer:
+	kfree(devices[minor].data);
+	return ret
+}
+
+static void destroy_membuf_device(int minor) {
+	if (!devices[minor].data) {
+		return;
+	}
+	device_destroy(devices[minor].dev);
+	cdev_del(&devices[minor].cdev);
+	kfree(devices[minor].data);
+	devices[minor] = NULL;
+}
+
+static int __init membuf_init(void)
+{
+	if (devices_number <= 0 || devices_number > MAX_DEVICES) {
+		pr_err("membuf: incorrect device number\n");
+		return -EINVAL;
+	}
+	if (base_device_size <= 0) {
+		pr_err("membuf: incorrect device size\n");
+		return -EINVAL;
+	}
+	int ret;
+    if ((ret = alloc_chrdev_region(&module_dev, 0, MAX_DEVICES, DEVICE_NAME)))
+	{
+		pr_err("membuf: failed to allocate region\n");
+		return ret;
+	}
+	membuf_class = class_create("membuf");
+	if (IS_ERR(membuf_class)) {
+		ret = PTR_ERR(membuf_class);
+		pr_err("membuf: failed to create class\n");
+		goto unregister_class;
+	}
+
+	int device_id = 0;
+	for (; device_id < devices_number; ++device_id) {
+		ret = create_membuf_device(device_id);
+		if (ret < 0) {
+			pr_err("membuf: failed to create device %d\n", device_id);
+			goto delete_devices;
+		}
+	}
+	return 0;
+
+delete_devices:
+	device_id--;
+	while (device_id > 0) {
+		destroy_membuf_device(device_id);
+		device_id--;
+	}
+unregister_class:
+	unregister_chrdev_region(module_dev, MAX_DEVICES);
 	return ret;
 }
 
 static void __exit membuf_exit(void)
 {
-	device_destroy(chrdev_class, dev);
-	class_destroy(chrdev_class);
-	cdev_del(&chrdev_cdev);
-	unregister_chrdev_region(dev, 1);
+	for (int i = 0; i < MAX_DEVICES; ++i) {
+		destroy_membuf_device(i);
+	}
 
+	class_destroy(membuf_class);
+	unregister_chrdev_region(module_dev, MAX_DEVICES);
 	pr_info("membuf: module unloaded\n");
 }
 
